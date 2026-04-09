786cee85b5d32776141985fff4b01ba60bd8dcdd
Esion <dongwon121322@gmail.com>
2026-04-09 16:19:45 +0900

    week06 numpy v0.2

--- week06.py
+++ week06.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-array01 = np.random.rand(2,3,3)
-print(array01)
-print(array01.shape, array01.dtype, array01.ndim, array01.size)
+array08 = np.random.rand(2,3,3)
+print(array08)
+print(array08.shape, array08.dtype, array08.ndim, array08.size)
